578e9a70b8d0918c427a3562fbf8bc9fe565f41d
franjlorenzo <63590328+franjlorenzo@users.noreply.github.com>
2020-05-22 00:32:41 -0300

    Tarea Fabrica Caños

---
+++ FabricaCaños/Fabrica_Caños/src/fabrica_caños/Fabrica_Caños.java
@@ -0,0 +1,9 @@
+package fabrica_caños;
+
+public class Fabrica_Caños {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ FabricaCaños/Fabrica_Caños/src/fabrica_caños/Material.java
@@ -0,0 +1,16 @@
+package fabrica_caños;
+
+public class Material {
+
+    private String descripcion;
+    private int codigo;
+    private float valorUnitario;
+
+    public float getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+}
