28a601a433e8f7c599e993af0a4c9ac6f321de12
franjlorenzo <63590328+franjlorenzo@users.noreply.github.com>
2020-05-25 11:08:18 -0300

    FabricaCaños

--- FabricaCaños/Fabrica_Caños/src/fabrica_caños/Material.java
+++ FabricaCaños/Fabrica_Caños/src/fabrica_caños/Material.java
@@ -2,7 +2,7 @@ package fabrica_caños;
 
 public class Material {
 
-    private String descripcion;
+    private String descripcionMaterial;
     private int codigo;
     private float valorUnitario;
 
@@ -12,5 +12,23 @@ public class Material {
 
     public int getCodigo() {
         return codigo;
+    }
+    
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+    
+    public void setDescripcionMaterial(String descripcionMaterial){
+        this.descripcionMaterial = descripcionMaterial;
+    }
+    
+    public void setValorUnitario(float valorUnitario){
+        this.valorUnitario = valorUnitario;
+    }
+    
+    public Material(int codigo, String descripcionMaterial, float valorUnitario){
+        this.codigo = codigo;
+        this.descripcionMaterial = descripcionMaterial;
+        this.valorUnitario = valorUnitario;
     }
 }
